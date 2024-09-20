a08f3b972b050942be60038d47902cc74ababe75
G626 <103378730+Krablante@users.noreply.github.com>
2024-09-20 21:41:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let timer = document.getElementById('timer');
+let startBtn = document.getElementById('startBtn');
+let pauseBtn = document.getElementById('pauseBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let intervalTime = undefined;
+
+function updateTime() {
+  seconds++;
+  if (seconds === 60) {
+    minutes++;
+    seconds = 0;
+  }
+  if (minutes === 60) {
+    hours++;
+    minutes = 0;
+  }
+  timer.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+startBtn.addEventListener('click', () => {
+  interval = setInterval(updateTime, 1000);
+  });
+  
+pauseBtn.addEventListener('click', () => {
+  clearInterval(interval);
+});
+
+resetBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  timer.textContent = '00:00:00';
+});
